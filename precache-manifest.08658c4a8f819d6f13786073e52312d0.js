e4c42874b8c60013c6f87993c8e39fdaeb994cf5
lasha nozadze <nozadze016@gmail.com>
2018-11-18 00:13:47 +0400

    Updates

---
+++ precache-manifest.08658c4a8f819d6f13786073e52312d0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "dca23cc8e84aefe2ad7a",
+    "url": "/contactmanager/static/js/main.dca23cc8.chunk.js"
+  },
+  {
+    "revision": "e61fa37cff944de67530",
+    "url": "/contactmanager/static/js/1.e61fa37c.chunk.js"
+  },
+  {
+    "revision": "dca23cc8e84aefe2ad7a",
+    "url": "/contactmanager/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "e61fa37cff944de67530",
+    "url": "/contactmanager/static/css/1.c7dcdc69.chunk.css"
+  },
+  {
+    "revision": "9d3c79be260f1b6c8898d3b5f9c0d381",
+    "url": "/contactmanager/index.html"
+  }
+];

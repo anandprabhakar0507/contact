741922c1178b4dcf5345d4b01300ee1cd3a02c15
lasha nozadze <nozadze016@gmail.com>
2018-11-18 01:00:51 +0400

    initial commit

---
+++ build/precache-manifest.dbb0e9e30e5f8353457eb70cb655d921.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fbf82b8ce2fdcf534fbc",
+    "url": "/contact/static/js/runtime~main.fbf82b8c.js"
+  },
+  {
+    "revision": "a28e1e42491e9bd57e68",
+    "url": "/contact/static/js/main.a28e1e42.chunk.js"
+  },
+  {
+    "revision": "da1c21a973e8c0df83a4",
+    "url": "/contact/static/js/1.da1c21a9.chunk.js"
+  },
+  {
+    "revision": "a28e1e42491e9bd57e68",
+    "url": "/contact/static/css/main.5bf5a46e.chunk.css"
+  },
+  {
+    "revision": "da1c21a973e8c0df83a4",
+    "url": "/contact/static/css/1.527be505.chunk.css"
+  },
+  {
+    "revision": "82f7ab153ebf3dd0d0cff35e5c5fb129",
+    "url": "/contact/index.html"
+  }
+];
